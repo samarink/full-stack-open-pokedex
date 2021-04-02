d66f8343f0e1979aa9622a28dff0c50963367034
Konstantin Samarin <konstsamari@gmail.com>
2021-04-02 20:08:28 +0700

    exercise 11.11

--- app.js
+++ app.js
@@ -7,5 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000') // eslint-disable-line
+  console.log('server started on port 5000'); // eslint-disable-line
+})
+
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
 })
